e171db637179aeac311b87f810162c2e96110006
Oscar Calvo <ocalvo@microsoft.com>
2025-04-04 12:04:10 -0700

    Add -Async parameter

--- Execute-Razzle.ps1
+++ Execute-Razzle.ps1
@@ -20,6 +20,7 @@ param (
   [switch]$nobtok,
   [switch]$gitVersionCheck,
   [switch]$autoMount,
+  [switch]$Async,
   [int]$commitLookUp=20,
   [switch]$fast = $true,
   [switch]$noprompt = $true,
@@ -74,26 +75,11 @@ if ($null -eq $enlistment)
 if (test-path $enlistment)
 {
   Set-Location $enlistment
-  pushd $enlistment
+  Push-Location $enlistment
 }
 
 if ($null -ne $env:_BuildArch) {$arch=$env:_BuildArch;}
 if ($null -ne $env:_BuildType) {$flavor=$env:_BuildType;}
-
-function global:Execute-OutsideRazzle
-{
-  param([ScriptBlock] $script)
-
-  Undo-Razzle;
-  try
-  {
-    & $script;
-  }
-  finally
-  {
-    Redo-Razzle;
-  }
-}
 
 Set-Alias UnRazzle Execute-OutsideRazzle -Scope Global;
 
@@ -110,7 +96,7 @@ function Get-RazzleProbeDir($kind, $srcDir)
 
 function Get-RazzleKind($srcDir)
 {
-  $kind = $razzleKind.Keys | where { (test-path (Get-RazzleProbeDir $_ $srcDir)) } | Select-Object -First 1
+  $kind = $razzleKind.Keys | Where-Object { (test-path (Get-RazzleProbeDir $_ $srcDir)) } | Select-Object -First 1
   return $kind
 }
 
@@ -126,7 +112,7 @@ function global:Get-RazzleProbes()
   }
   else
   {
-    if (($enlistment -ne $null) -and (test-path $enlistment))
+    if (($null -ne $enlistment) -and (test-path $enlistment))
     {
       $razzleProbe += $enlistment
       return $razzleProbe
@@ -138,42 +124,16 @@ function global:Get-RazzleProbes()
 
 function __Get-BranchName($razzleDirName)
 {
-  $branch = (git branch | where { $_.StartsWith("*") } | select -first 1 )
-  if ($branch -ne $null)
+  $branch = (git branch | Where-Object { $_.StartsWith("*") } | select -first 1 )
+  if ($null -ne $branch)
   {
     $branch = $branch.Split("/") | select -last 1
-    if ($branch -ne $null)
+    if ($null -ne $branch)
     {
       return $branch;
     }
   }
   return $razzleDirName;
-}
-
-function global:New-RazzleLink($linkName, $binaries)
-{
-  if ($noSymbolicLinks.IsPresent)
-  {
-     return;
-  }
-  Write-Verbose "Linking $linkName -> $binaries ..."
-
-  if (!(test-path $binaries))
-  {
-     Write-Verbose "Making new dir $binaries"
-     mkdir $binaries | Out-Null
-  }
-
-  $currentTarget = $null
-  if (test-path $linkName)
-  {
-     $currentTarget = (Get-Item $linkName).Target
-  }
-  if (($currentTarget -eq $null) -or ($currentTarget -ne $binaries))
-  {
-     Write-Verbose "Making new link $linkName -> $binaries"
-     gsudo New-Item $linkName -ItemType SymbolicLink -Target $binaries -Force
-  }
 }
 
 function global:Get-BranchCustomId()
@@ -185,89 +145,19 @@ function global:Get-BranchCustomId()
     }
 }
 
-function global:Retarget-Razzle
-{
-    Write-Verbose "Retarget-Razzle"
+function Invoke-RazzleAsync {
 
-    if ($noSymbolicLinks.IsPresent) {
-      return;
-    }
-
-    Write-Verbose ("Retargeting common paths")
-
-    New-RazzleLink "c:\Symbols" "$binariesPrefix\Symbols"
-    New-RazzleLink "c:\Symcache" "$binariesPrefix\Symbols"
-    New-RazzleLink "c:\Sym" "$binariesPrefix\Symbols"
-    #New-RazzleLink $env:temp "$binariesPrefix\Temp"
-    New-RazzleLink $env:HOMEDRIVE$env:HOMEPATH\.nuget "$binariesPrefix\NuGet"
-    New-RazzleLink "c:\Temp" "$binariesPrefix\Temp"
-    New-RazzleLink "c:\Logs" "$binariesPrefix\Logs"
-    New-RazzleLink "c:\CrashDumps" "$binariesPrefix\CrashDumps"
-    New-RazzleLink "c:\ProgramData\dbg\Sym" "$binariesPrefix\Symbols"
-    New-RazzleLink "c:\ProgramData\dbg\Src" "$binariesPrefix\src"
-
-    Write-Verbose ("Retargeting done")
 }
 
-function global:Retarget-OSRazzle($binariesRoot, $srcRoot = $env:OSBuildRoot)
-{
-    Write-Verbose "Retarget-OSRazzle"
-
-    if ($noSymbolicLinks.IsPresent) {
-      return;
+function Execute-FastRazzle {
+  Write-Verbose "Fast razzle using $env:EnlistmentEnv"
+  if (Test-path $env:EnlistmentEnv) {
+    Get-Content $env:EnlistmentEnv | ConvertFrom-Json |% { $k=$_.Name; $v=$_.Value; Set-Item -Path env:$k -Value $v }
+    $setRazzle = "$srcDir/utilities/psrazzle/setrazzle.ps1"
+    if (Test-Path $setRazzle) {
+      .$setRazzle
     }
-
-    Write-Verbose ("Retargeting $srcRoot -> $binariesRoot")
-
-    Push-Location ($srcRoot+"\src")
-    $binRoot = $srcRoot.Replace("f:","w:")
-    $binRoot = $binRoot.Replace("F:","w:")
-    $binRoot = $binRoot.Replace("c:\src","w:")
-    $binRoot = $binRoot.Replace("C:\src","w:")
-    Write-Verbose "Branch binRoot is $binRoot"
-    Pop-Location
-
-    New-RazzleLink "$binariesPrefix\os" $binRoot
-    New-RazzleLink ($srcRoot+"\bin") ($binRoot+"\bin")
-    New-RazzleLink ($srcRoot+"\bldcache") ($binRoot+"\bldcache")
-    New-RazzleLink ($srcRoot+"\bldout") ($binRoot+"\bldout")
-    New-RazzleLink ($srcRoot+"\cdg") ($binRoot+"\cdg")
-    New-RazzleLink ($srcRoot+"\intl") ($binRoot+"\intl")
-    New-RazzleLink ($srcRoot+"\engcache") ($binRoot+"\engcache")
-    New-RazzleLink ($srcRoot+"\pgo") ($binRoot+"\pgo")
-    New-RazzleLink ($srcRoot+"\public") ($binRoot+"\public")
-    New-RazzleLink ($srcRoot+"\pubpkg") ($binRoot+"\pubpkg")
-    New-RazzleLink ($srcRoot+"\obj") ($binRoot+"\obj")
-    New-RazzleLink ($srcRoot+"\osdep") ($binRoot+"\osdep")
-    New-RazzleLink ($srcRoot+"\out") ($binRoot+"\out")
-    New-RazzleLink ($srcRoot+"\Temp") ($binRoot+"\Temp")
-    New-RazzleLink ($srcRoot+"\tools") ($binRoot+"\tools")
-    New-RazzleLink ($srcRoot+"\utilities") ($binRoot+"\utilities")
-
-    New-RazzleLink ($binRoot+"\src") ($srcRoot+"\src")
-    New-RazzleLink ($srcRoot+"\TestPayload") ($binRoot+"\TestPayload")
-
-    Write-Verbose ("Retargeting done")
-}
-
-function global:Retarget-LiftedRazzle
-{
-    if ($noSymbolicLinks.IsPresent) {
-      return;
-    }
-
-    $_srcName = Split-Path $enlistment -Leaf
-    $binRoot = ($binariesPrefix+"\"+$_srcName)
-    $srcRoot = ("c:\src\"+$_srcName)
-    Write-Verbose "Branch binRoot is $binRoot, srcRoot is $srcRoot"
-
-    New-RazzleLink ($srcDir+"\packages") ("$binariesPrefix\NuGet\packages")
-    New-RazzleLink ($srcDir+"\buildOutput") ($binRoot)
-    New-RazzleLink ($srcDir+"\TestPayload") ($binRoot+"\TestPayLoad")
-    New-RazzleLink ($srcDir+"\bin") ($binRoot+"\bin")
-    New-RazzleLink ($srcDir+"\obj") ($binRoot+"\obj")
-    New-RazzleLink ($srcDir+"\temp") ($binRoot+"\temp")
-    New-RazzleLink ($srcDir+"\log") ($binRoot+"\out")
+  }
 }
 
 function Execute-Razzle-Internal($flavor="chk",$arch="x86",$enlistment)
@@ -279,8 +169,6 @@ function Execute-Razzle-Internal($flavor="chk",$arch="x86",$enlistment)
   }
 
   $popDir = Get-Location
-
-  #Undo-Razzle
 
   $razzleProbe = (Get-RazzleProbes)
   $binaries = $binariesPrefix
@@ -345,25 +233,11 @@ function Execute-Razzle-Internal($flavor="chk",$arch="x86",$enlistment)
             $env:RazzleOptions += " temp " + $tempDir
           }
 
-          Retarget-Razzle
-
           Write-Verbose "Adding razzle commands line arguments"
 
           if ($nobtok.IsPresent)
           {
             $env:RazzleOptions += " no_bl_ok "
-          }
-
-          $phoneOptions = ""
-          if ( $kind -eq "Phone" )
-          {
-            $uConfig = ($ddDir + '\DefaultWindowsSettings.uconfig')
-            if (test-path $uConfig)
-            {
-              $phoneOptions += " uconfig=" + $uConfig + " "
-            }
-
-            $phoneOptions += (" UConfig_Razzle_Parameters=`""+$env:RazzleOptions+"`" ")
           }
 
           $env:_XROOT = $srcDir.Trim("\")
@@ -373,56 +247,35 @@ function Execute-Razzle-Internal($flavor="chk",$arch="x86",$enlistment)
           $commitId = (git rev-parse HEAD)
           if ($fast) {
              $lastCommits = git log -n $commitLookUp --pretty=format:"%H"
-             $lastEnvId = $lastCommits | where { Test-Path "$env:_XOSROOT\$_.$arch.$flavor.env.json" } | Select -First 1
+             $lastEnvId = $lastCommits | Where-Object { Test-Path "$env:_XOSROOT\$_.$arch.$flavor.env.json" } | Select -First 1
              if (($null -ne $lastEnvId) -and ($lastEnvId -ne $commitId)) {
                  $commitId = $lastEnvId
              }
           }
           $env:EnlistmentEnv = "$env:_XOSROOT\$commitId.$arch.$flavor.env.json"
 
-          if ( ($null -ne $env:EnlistmentEnv) -and (Test-Path $env:EnlistmentEnv) -and ($fast) ) {
-            Write-Verbose "Fast razzle using $env:EnlistmentEnv"
-            Get-Content $env:EnlistmentEnv | ConvertFrom-Json |% { $k=$_.Name; $v=$_.Value; Set-Item -Path env:$k -Value $v }
-            $setRazzle = "$srcDir/utilities/psrazzle/setrazzle.ps1"
-            if (Test-Path $setRazzle) {
-              .$setRazzle
-            }
+          $perlCmd = "$env:_XOSROOT\tools\perl\bin\perl.exe"
+          $perlExists = (Test-Path $perlCmd)
+          Push-Location "$env:_XOSROOT\src"
+          if ($Async) {
+            Execute-FastRazzle
+            Set-Location $PSScriptRoot
+            $global:RazzleJob = Start-ThreadJob -ScriptBlock {
+              param(
+                $razzle,
+                $arch,
+                $flavor,
+                $binaries,
+                $depotRoot,
+                $noPrompt,
+                $noSymbolicLinks,
+                $args)
+              .".\Execute-RealRazzle.ps1" -razzle $razzle -arch $arch -flavor $flavor -binaries $binaries -depotRoot $depotRoot -noPrompt:$noPrompt -noSymbolicLinks:$noSymbolicLinks @args
+            } -ArgumentList ($razzle, $arch, $flavor, $binaries, $depotRoot, ($noPrompt.IsPresent), $true, $args)
+          } elseif ( ($null -ne $env:EnlistmentEnv) -and (Test-Path $env:EnlistmentEnv) -and ($fast) -and ($perlExists)) {
+            Execute-FastRazzle
           } else {
-            Remove-Item "$env:_XOSROOT\*.$arch.$flavor.env.json"
-            if ( $kind -eq "Phone" ) {
-              Write-Verbose "Phone: $razzle $device $arch$flavor $phoneOptions"
-              .$razzle $device ($arch+$flavor) $phoneOptions @args
-            }
-            elseif ( $kind -eq "Lifted" ) {
-              Write-Verbose "Lifted: $razzle $initParams"
-              Retarget-LiftedRazzle
-              Push-Location $env:SDXROOT
-              Enter-VSShell -vsVersion $vsVersion -vsYear $vsYear
-              Write-Verbose ".$razzle $arch$flavor"
-              $initParams = (($arch+$flavor),"/2019")
-              $initParams = (($arch+$flavor))
-              Invoke-CmdScript -script $razzle -parameters $initParams
-              .$PSScriptRoot\MSBuild-Alias.ps1 -msBuildAlias
-            }
-            else {
-              Write-Verbose "Windows: $razzle"
-              Retarget-OSRazzle $binaries (Get-item $depotRoot).Parent.FullName
-              $arch = $arch.Replace("x64","amd64")
-
-              $setRazzlePs1Dir = "$env:_XROOT\developer\$env:USERNAME"
-              Write-Verbose "Reseting setrazzle.ps1: $setRazzlePs1Dir"
-              mkdir $setRazzlePs1Dir -ErrorAction Ignore | Out-Null
-              $setRazzlePs1 = "$setRazzlePs1Dir\setrazzle.ps1"
-              Set-Content "" -Path $setRazzlePs1
-              if ($noPrompt) {
-                $args += "noprompt"
-              }
-              Write-Verbose ".$razzle $flavor $arch $env:RazzleOptions $args"
-              .$razzle $flavor $arch $env:RazzleOptions @args
-            }
-            $envData = Get-ChildItem env: | Select -Property Name,Value
-            $envData | ConvertTo-Json -Depth 2 | Set-Content $env:EnlistmentEnv
-            Write-Verbose "Stored razzle environment in '$env:EnlistmentEnv'"
+            ."$PSScriptRoot\Execute-RealRazzle.ps1" -razzle:$razzle -arch $arch -flavor $flavor -binaries $binaries -depotRoot $depotRoot -noPrompt:($noPrompt.IsPresent) -noSymbolicLinks:($noSymbolicLinks.IsPresent) @args
           }
 
           Pop-Location
---
+++ Execute-RealRazzle.ps1
@@ -0,0 +1,125 @@
+[CmdLetBinding()]
+param(
+  $razzle,
+  $arch,
+  $flavor,
+  $binaries,
+  $depotRoot,
+  [switch]$noPrompt,
+  [switch]$noSymbolicLinks)
+
+function New-RazzleLink($linkName, $binaries)
+{
+  if ($noSymbolicLinks.IsPresent)
+  {
+     return;
+  }
+
+  Write-Verbose "Linking $linkName -> $binaries ..."
+
+  if (!(test-path $binaries))
+  {
+     Write-Verbose "Making new dir $binaries"
+     mkdir $binaries | Out-Null
+  }
+
+  $currentTarget = $null
+  if (test-path $linkName)
+  {
+     $currentTarget = (Get-Item $linkName).Target
+  }
+  if (($currentTarget -eq $null) -or ($currentTarget -ne $binaries))
+  {
+     Write-Verbose "Making new link $linkName -> $binaries"
+     gsudo New-Item $linkName -ItemType SymbolicLink -Target $binaries -Force
+  }
+}
+
+function Retarget-OSRazzle($binariesRoot, $srcRoot = $env:OSBuildRoot)
+{
+    Write-Verbose "Retarget-OSRazzle"
+
+    if ($noSymbolicLinks.IsPresent) {
+      return;
+    }
+
+    Write-Verbose ("Retargeting $srcRoot -> $binariesRoot")
+
+    Push-Location ($srcRoot+"\src")
+    $binRoot = $srcRoot.Replace("f:","w:")
+    $binRoot = $binRoot.Replace("F:","w:")
+    $binRoot = $binRoot.Replace("c:\src","w:")
+    $binRoot = $binRoot.Replace("C:\src","w:")
+    Write-Verbose "Branch binRoot is $binRoot"
+    Pop-Location
+
+    New-RazzleLink "$binariesPrefix\os" $binRoot
+    New-RazzleLink ($srcRoot+"\bin") ($binRoot+"\bin")
+    New-RazzleLink ($srcRoot+"\bldcache") ($binRoot+"\bldcache")
+    New-RazzleLink ($srcRoot+"\bldout") ($binRoot+"\bldout")
+    New-RazzleLink ($srcRoot+"\cdg") ($binRoot+"\cdg")
+    New-RazzleLink ($srcRoot+"\intl") ($binRoot+"\intl")
+    New-RazzleLink ($srcRoot+"\engcache") ($binRoot+"\engcache")
+    New-RazzleLink ($srcRoot+"\pgo") ($binRoot+"\pgo")
+    New-RazzleLink ($srcRoot+"\public") ($binRoot+"\public")
+    New-RazzleLink ($srcRoot+"\pubpkg") ($binRoot+"\pubpkg")
+    New-RazzleLink ($srcRoot+"\obj") ($binRoot+"\obj")
+    New-RazzleLink ($srcRoot+"\osdep") ($binRoot+"\osdep")
+    New-RazzleLink ($srcRoot+"\out") ($binRoot+"\out")
+    New-RazzleLink ($srcRoot+"\Temp") ($binRoot+"\Temp")
+    New-RazzleLink ($srcRoot+"\tools") ($binRoot+"\tools")
+    New-RazzleLink ($srcRoot+"\utilities") ($binRoot+"\utilities")
+
+    New-RazzleLink ($binRoot+"\src") ($srcRoot+"\src")
+    New-RazzleLink ($srcRoot+"\TestPayload") ($binRoot+"\TestPayload")
+
+    Write-Verbose ("Retargeting done")
+}
+
+function Retarget-Razzle
+{
+    Write-Verbose "Retarget-Razzle"
+
+    if ($noSymbolicLinks.IsPresent) {
+      return;
+    }
+
+    Write-Verbose ("Retargeting common paths")
+
+    New-RazzleLink "c:\Symbols" "$binariesPrefix\Symbols"
+    New-RazzleLink "c:\Symcache" "$binariesPrefix\Symbols"
+    New-RazzleLink "c:\Sym" "$binariesPrefix\Symbols"
+    #New-RazzleLink $env:temp "$binariesPrefix\Temp"
+    New-RazzleLink $env:HOMEDRIVE$env:HOMEPATH\.nuget "$binariesPrefix\NuGet"
+    New-RazzleLink "c:\Temp" "$binariesPrefix\Temp"
+    New-RazzleLink "c:\Logs" "$binariesPrefix\Logs"
+    New-RazzleLink "c:\CrashDumps" "$binariesPrefix\CrashDumps"
+    New-RazzleLink "c:\ProgramData\dbg\Sym" "$binariesPrefix\Symbols"
+    New-RazzleLink "c:\ProgramData\dbg\Src" "$binariesPrefix\src"
+
+    Write-Verbose ("Retargeting done")
+}
+
+Write-Verbose "Execute-RealRazzle"
+
+Remove-Item "$env:_XOSROOT\*.$arch.$flavor.env.json"
+
+Retarget-Razzle
+Retarget-OSRazzle $binaries (Get-item $depotRoot).Parent.FullName
+$arch = $arch.Replace("x64","amd64")
+
+$setRazzlePs1Dir = "$env:_XROOT\developer\$env:USERNAME"
+Write-Verbose "Reseting setrazzle.ps1: $setRazzlePs1Dir"
+mkdir $setRazzlePs1Dir -ErrorAction Ignore | Out-Null
+$setRazzlePs1 = "$setRazzlePs1Dir\setrazzle.ps1"
+Set-Content "" -Path $setRazzlePs1
+if ($noPrompt) {
+  $args += "noprompt"
+}
+Write-Verbose ".$razzle $flavor $arch $env:RazzleOptions $args"
+.$razzle $flavor $arch $env:RazzleOptions @args
+
+$envData = Get-ChildItem env: | Select -Property Name,Value
+$envData | ConvertTo-Json -Depth 2 | Set-Content $env:EnlistmentEnv
+Write-Verbose "Stored razzle environment in '$env:EnlistmentEnv'"
+
